8f97a0ad999d422482db0ade3180ae1e0cd76f38
Ryan Alves <ra546038@gmail.com>
2023-06-16 17:21:36 -0300

    Faltando animações e comandos

---
+++ app.js
@@ -0,0 +1,23 @@
+const img1 = document.getElementById('teste1')
+const img2 = document.getElementById('teste2')
+
+const image1 = document.getElementById('img1')
+const image2 = document.getElementById('img2')
+
+
+function mostraImagem1 () {
+    
+
+    image.classList.add('show')
+    
+}
+
+function mostraImagem2 () {
+    
+    image.classList.add('show')
+    
+}
+
+
+img1.addEventListener("click", mostraImagem1)
+img2.addEventListener("click", mostraImagem2)
