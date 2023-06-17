6c4cbe624f3997fcb0d08a0aeaa602da2ca7f422
RyanAlvesz <ra546038@gmail.com>
2023-06-17 20:31:52 -0300

    Arrumando link

--- img-slides.js
+++ img-slides.js
@@ -8,7 +8,7 @@ const changeimg = () => {
 const change = {
     'img1': () => img.src = './img/protetor-solar.jpeg',
     'img2': () => img.src = './img/protetor-solar-1.png',
-    'img3': () => img.src = '/img/flor.png'
+    'img3': () => img.src = './img/flor.png'
 }
 
 
